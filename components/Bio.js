6037183939a0e8ed56ea86a147e3dc478e66a7d7
Sadie Allen <sadiela@Sadies-MacBook-Pro.local>
2022-08-14 18:09:12 -0400

    inline styles try again

--- components/Bio.js
+++ components/Bio.js
@@ -7,7 +7,7 @@ export default class Bio extends Component {
         return (
             <div>
             <h1 className="title">Bio</h1>
-            <p fontFamily='monospace'>
+            <p style={{ fontFamily: 'monospace' }}>
                 I am a second-year computer engineering PhD student at Boston University interested primarily in applying deep learning methods to music. I want to design tools that use machine learning to assist artists and producers in the creative process. My current research focuses on controllable symbolic music generation. I am also very interested in the ethics and fairness of machine learning.  
             </p>
             <p fontFamily='monospace'>
