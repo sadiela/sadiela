46359a1b73cc95e37fde8150ad37004967fd709a
Sadie Allen <sadiela@bu.edu>
2022-10-08 15:37:41 -0400

    another book and descriptions

--- pages/Bio.js
+++ pages/Bio.js
@@ -16,8 +16,7 @@ export default class Bio extends Component {
                     src="/sadie_headshot.jpg" // Route of the image file
                     height={144} // Desired size with correct aspect ratio
                     width={144} // Desired size with correct aspect ratio
-                    alt="Sadie Allen"
-                    />
+                    alt="Sadie Allen"/>
                     <h1 className="title" style={{ fontFamily: 'monospace' }}>Bio</h1>
                     <p style={{ fontFamily: 'monospace' }}>
                         I am a second-year computer engineering PhD student at Boston University interested primarily in applying deep learning methods to music. I want to design tools that use machine learning to assist artists and producers in the creative process. My current research focuses on controllable symbolic music generation. I am also very interested in the ethics and fairness of machine learning.  
--- pages/Miscellaneous.js
+++ pages/Miscellaneous.js
@@ -24,11 +24,13 @@ export default class Miscellaneous extends Component {
                 <p></p>            
                 <a target="_blank" href="https://open.spotify.com/episode/1HrqYxTctwCWM0f1ie4m51?si=dd1d2c9f19b84d9b">Hidden Brain: Edge Effect</a>
                 <h2 style={{ fontFamily: 'monospace' }}>Books I've greatly enjoyed</h2>
-                <a target="_blank" href="https://wwnorton.com/books/9780393652208">Useful Delusions - Shankar Vedantam</a>
+                <p><a style={{ paddingRight: '3px', paddingLeft:'3px' }} target="_blank" href="https://wwnorton.com/books/9780393652208">Useful Delusions - Shankar Vedantam</a>: The ways in which our own brain deceives us and how we can leverage them.</p>
                 <p></p>
-                <a target="_blank" href="https://www.goodreads.com/book/show/53487334-the-best-of-me">The Best of Me - David Sedaris</a>
+                <p><a style={{ paddingRight: '3px', paddingLeft:'3px' }} target="_blank" href="https://www.goodreads.com/book/show/53487334-the-best-of-me">The Best of Me - David Sedaris</a>: Hilarious and heartbreaking essays.</p>
                 <p></p>            
-                <a target="_blank" href="https://www.goodreads.com/en/book/show/56353777-the-cabinet#CommunityReviews">The Cabinet - Un Su Kim</a>
+                <p><a style={{ paddingRight: '3px', paddingLeft:'3px' }} target="_blank" href="https://www.goodreads.com/en/book/show/56353777-the-cabinet#CommunityReviews">The Cabinet - Un Su Kim</a>: Surreal mosaic novel about strange people who seem to have evolved past human norms.</p>
+                <p></p> 
+                <p><a style={{ paddingRight: '3px', paddingLeft:'3px' }} target="_blank" href="https://carolinecriadoperez.com/book/invisible-women/">Invisible Women - Caroline Criado Perez</a>: Compelling evidence of the gender data gap and the damage it causes to women and society as a whole.</p>
             </Layout>
             </div>
         )
