a7bad99b4e1d8e8d4384ccec747b48d3512a2ad1
sadiela <sadiela@bu.edu>
2025-02-06 20:00:33 -0500

    added all cheatsheet pdfs

--- pages/CheatSheets.js
+++ pages/CheatSheets.js
@@ -19,6 +19,22 @@ export default class CheatSheets extends Component {
                         Throughout college, I made a bunch of guides or "cheat sheets" for my engineering classes to help me study for tests. I've uploaded PDF versions as references for anyone who wants to use them! Hopefully I can type them up at some point as well.
                     </p>
                     <p></p>
+                    <p><a href="cheatsheets/advanced_optimization.pdf">Advanced Optimization</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/data_structures_algorithms.pdf">Data Structures and Algorithms</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/differential_equations.pdf">Differential Equations</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/latin_grammar.pdf">Latin Grammar</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/linear_optimization.pdf">Linear Optimization</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/machine_learning.pdf">Machine Learning</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/multivariate_calculus.pdf">Multivariate Calculus</a></p>
+                    <p></p>
+                    <p><a href="cheatsheets/data_structures_algorithms.pdf"> Data Structures and Algorithms</a></p>
+                    <p></p>
                     <p><a href="cheatsheets/data_structures_algorithms.pdf"> Data Structures and Algorithms</a></p>
                 </Layout>
             </div>
