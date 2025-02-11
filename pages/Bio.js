19e0061ce7f42efbd9fc588f40f45b635df144c1
sadiela <sadiela@bu.edu>
2025-02-11 11:14:49 -0500

    bio update

--- pages/Bio.js
+++ pages/Bio.js
@@ -24,7 +24,7 @@ export default class Bio extends Component {
                 </div>
                 <div class="column">
                     <p style={{ fontFamily: 'monospace' }}>
-                        I am a recent M.S. graduate from Boston University with a degree in Computer Engineering. I am seeking a full-time position in a machine learning or software engineering role.   
+                        I am a recent graduate with a Master’s in Computer Engineering from Boston University, eager to apply my skills and passion for technology to a career in a machine learning or software engineering role. As a driven, detail-oriented, and curious individual, I’m always looking for ways to deepen my understanding of computing and push the boundaries of what’s possible. I thrive in environments that challenge me and encourage continuous learning, whether it's coding, collaborating with a team, or diving into new systems.
                     </p>
                     <p style={{ fontFamily: 'monospace' }}>
                         In my free time, I enjoy running, hiking, singing, and writing and producing music. 
