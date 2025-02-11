b6770c942a15e04f3b3eb668a281a9175a8f2c7a
sadiela <sadiela@bu.edu>
2025-02-11 11:12:19 -0500

    rearranging nav bar

--- components/navigate.js
+++ components/navigate.js
@@ -13,11 +13,11 @@ export const linkStyle = {
 export default function Navigate() {
     return <h3 className={style.navigate}>
         <Link className={style.linkclass} href="/Bio">Bio</Link> 
-        <Link className={style.linkclass}  href="/Publications">Publications</Link>    
         <Link className={style.linkclass}  href="/PersonalProjects">Personal Projects</Link> 
+        <Link className={style.linkclass}  href="/Publications">Publications</Link>   
+        <Link className={style.linkclass}  href="/CheatSheets">Cheat Sheets</Link>       
         <Link className={style.linkclass}  href="/Music">Music</Link>  
-        <Link className={style.linkclass}  href="/CheatSheets">Cheat Sheets</Link>      
         <Link className={style.linkclass}  href="/Miscellaneous">Miscellaneous</Link> 
-        <Link className={style.linkclass}  href="/Contact">Contact me</Link>         
+        <Link className={style.linkclass}  href="/Contact">Contact Me</Link>         
     </h3>;
 }
